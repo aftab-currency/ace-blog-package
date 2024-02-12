39a5b20443c7cbfcc78f7a488a8ab17e48d5eafc
asc-muhammadyousaf <muhammad.yousaf.asc@aftabcurrency.eu>
2024-02-12 21:15:43 +0500

    ok

--- src/Controllers/ACEBlogController.php
+++ src/Controllers/ACEBlogController.php
@@ -1,10 +1,8 @@
 <?php
 
 namespace ACE\ACEBlog\Controllers;
-use Yousafitpro\PhotoLibrary\models\ucGalleryFile;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\File;
 class ACEBlogController extends Controller
 {
   //asdasdasdasdas
--- src/routes/web.php
+++ src/routes/web.php
@@ -1,22 +1,12 @@
 <?php
 
 
-// Route::group([
-//     'namespace' => 'Yousafitpro\PhotoLibrary\Controllers',
-//     'prefix' => 'photo-library',
-// ], function () {
-//     Route::get('', 'UcPhotoLibraryController@index');
-// });
-
-
-//adasd
-//asdasd
 Route::prefix('ACE-Blog')
     ->namespace('ACE\ACEBlog\Controllers')
     ->group(function () {
         Route::get('/', 'ACEBlogController@index')->name('ACEBlog.index');
 
-///adasd
+
     });
 
-//asdasd
+
