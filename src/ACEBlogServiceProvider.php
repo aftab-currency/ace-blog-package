3af2fc47115bbc12dd4f918fa188358580baabcd
asc-muhammadyousaf <muhammad.yousaf.asc@aftabcurrency.eu>
2024-02-13 14:44:43 +0500

    new updates

--- src/ACEBlogServiceProvider.php
+++ src/ACEBlogServiceProvider.php
@@ -5,6 +5,13 @@
     class ACEBlogServiceProvider extends ServiceProvider {
         public function boot()
         {
+            $this->publishes([
+                __DIR__.'/config/ACEBlog-Config.php' => config_path('ACEBlog-Config.php'),
+                __DIR__.'/migrations' => database_path('migrations'),
+                __DIR__.'/resources/css' => public_path('vendor/ACEBlog/css'),
+                __DIR__.'/resources/js' => public_path('vendor/ACEBlog/js'),
+            ], 'ACEBlog');
+
             //gjhgjhgasdasdasd
             $this->loadRoutesFrom(__DIR__.'/routes/web.php');
             $this->loadViewsFrom(__DIR__.'/resources/views', 'ACEBlog');
