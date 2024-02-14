b5ba45bbb46b2c600ccc2b6a85754276f645903e
asc-muhammadyousaf <muhammad.yousaf.asc@aftabcurrency.eu>
2024-02-14 22:06:27 +0500

    new updates

--- src/commands/ACEBlogInstallCommand.php
+++ src/commands/ACEBlogInstallCommand.php
@@ -16,15 +16,15 @@ class ACEBlogInstallCommand extends Command
 
 
 
-        // Artisan::call('migrate --path=database/migrations/2024_02_13_000000_create_ace_blog_languages_table.php');
-        // Artisan::call('migrate --path=database/migrations/2024_02_13_100320_create_ace_blog_categories_table.php');
-        // Artisan::call('migrate --path=database/migrations/2024_02_13_200000_create_ace_blog_categories_translations_table.php');
-        // Artisan::call('migrate --path=database/migrations/2024_02_13_300000_create_ace_blog_comments_table.php');
-        // Artisan::call('migrate --path=database/migrations/2024_02_13_400000_create_ace_blog_configurations_table.php');
-        // Artisan::call('migrate --path=database/migrations/2024_02_13_500000_create_ace_blog_post_categories_table.php');
-        // Artisan::call('migrate --path=database/migrations/2024_02_13_600000_create_ace_blog_post_translations_table.php');
-        // Artisan::call('migrate --path=database/migrations/2024_02_13_700000_create_ace_blog_posts_table.php');
-        // Artisan::call('migrate --path=database/migrations/2024_02_13_800000_create_ace_blog_uploaded_images_table.php');
+        Artisan::call('migrate --path=database/migrations/2024_02_13_000000_create_ace_blog_languages_table.php');
+        Artisan::call('migrate --path=database/migrations/2024_02_13_100320_create_ace_blog_categories_table.php');
+        Artisan::call('migrate --path=database/migrations/2024_02_13_200000_create_ace_blog_categories_translations_table.php');
+        Artisan::call('migrate --path=database/migrations/2024_02_13_300000_create_ace_blog_comments_table.php');
+        Artisan::call('migrate --path=database/migrations/2024_02_13_400000_create_ace_blog_configurations_table.php');
+        Artisan::call('migrate --path=database/migrations/2024_02_13_500000_create_ace_blog_post_categories_table.php');
+        Artisan::call('migrate --path=database/migrations/2024_02_13_600000_create_ace_blog_post_translations_table.php');
+        Artisan::call('migrate --path=database/migrations/2024_02_13_700000_create_ace_blog_posts_table.php');
+        Artisan::call('migrate --path=database/migrations/2024_02_13_800000_create_ace_blog_uploaded_images_table.php');
         Artisan::call('optimize:clear');
         Artisan::call('storage:link');
        
