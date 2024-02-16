faa7129eafd79c4691de18d45c3bb8242ea87433
asc-muhammadyousaf <muhammad.yousaf.asc@aftabcurrency.eu>
2024-02-16 17:25:56 +0500

    new updates

--- src/helpers/main.php
+++ src/helpers/main.php
@@ -66,7 +66,7 @@ if (! function_exists('aceblog_get_image_url')) {
         $image=AceBlogUploadedImage::where('id',$id)->withTrashed()->first();
          if($image==null)
          {
-            return '';
+            return 'https://static.vecteezy.com/system/resources/thumbnails/004/141/669/small_2x/no-photo-or-blank-image-icon-loading-images-or-missing-image-mark-image-not-available-or-image-coming-soon-sign-simple-nature-silhouette-in-frame-isolated-illustration-vector.jpg';
          }
         $url=$image->$size;
         if($image->disk=='public')
